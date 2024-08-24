62dde61703d19f7e3a80a1f06ba9fbf211253afa
abelgeorgeantony <abelgeorgeantony@gmail.com>
2024-08-25 01:05:44 +0530

    Some changes

--- scripts/installapps.sh
+++ scripts/installapps.sh
@@ -3,10 +3,13 @@
 declare -a apt_apps
 declare -a snap_apps
 declare -a flatpak_apps
-apt_apps+=("snapd", "flatpak", "mpv", "gimp", "neovim", "vim", "conky")
+apt_apps+=("zip", "unzip", "dconf-editor", "snapd", "flatpak", "mpv", "gimp", "neovim", "vim", "conky")
 snap_apps+=("code --classic", "scrcpy", "steam", "telegram-desktop")
 flatpak_apps+=("net.lutris.Lutris")
-#Apps to download from source:
+#Apps to download from other source:
 ##	obs studio
+##	virtualbox
 ##	zed
 ##	chrome
+##	Genshin Impact
+##	TLauncher Minecraft
