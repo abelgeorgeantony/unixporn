99c35757a95d65ee2150e99e56e2c1a5b93be94a
abelgeorgeantony <abelgeorgeantony@gmail.com>
2024-08-25 01:24:25 +0530

    Some change

--- scripts/installapps.sh
+++ scripts/installapps.sh
@@ -3,7 +3,7 @@
 declare -a apt_apps
 declare -a snap_apps
 declare -a flatpak_apps
-apt_apps+=("zip", "unzip", "dconf-editor", "snapd", "flatpak", "mpv", "gimp", "neovim", "vim", "conky")
+apt_apps+=("zip", "unzip", "dconf-editor", "snapd", "flatpak", "mpv", "gimp", "tmux", "neovim", "vim", "conky", "gnome-multi-writer")
 snap_apps+=("code --classic", "scrcpy", "steam", "telegram-desktop")
 flatpak_apps+=("net.lutris.Lutris")
 #Apps to download from other source:
